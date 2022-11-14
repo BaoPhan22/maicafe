032eafac2a2a419d1fd17d2d1eb1626b4cc3e052
BaoPhan22 <93857238+BaoPhan22@users.noreply.github.com>
2022-11-14 15:18:11 +0700

    Add files via upload

---
+++ view/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <h2>Header</h2>
+    </header>
+    <nav>
+        <ul>
+            <a href="index.php?act=about">Giới thiệu</a>
+        </ul>
+    </nav>
---
+++ view/index.php
@@ -0,0 +1,12 @@
+<?php
+include 'header.php';
+if (isset($_GET['act']) && ($_GET['act'])) {
+    switch ($_GET['act']) {
+        case ['about']:
+            include 'about.php';
+            break;
+    }
+} else {
+    include 'body.php';
+}
+include 'footer.php';
